c7aa870194d99254e16d4207674408e3dead3607
Moid3563 <114673597+Moid3563@users.noreply.github.com>
2023-03-14 13:20:46 +0530

    Add files via upload

---
+++ TodoList.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { useState } from 'react';
+import './todo.css';
+
+function TodoList() {
+    const [activity, setActivity] = useState("");
+    const [listData, setlistData] = useState([]);
+
+    function addActivity(){
+    //     setlistData([...listData, activity])
+    //     console.log(listData)
+    if(addActivity != ""){
+
+    
+    setlistData((listData)=>{
+    
+        const updatedList =([...listData, activity])
+        console.log(updatedList)
+        setActivity('');
+        return updatedList
+        
+      })
+        // const updatedList =([...listData, activity])
+        // console.log(updatedList)
+        // setActivity('');
+        // return updatedList
+    }
+}
+
+function removeActivity(i){
+  const updatedListData = listData.filter((elem, id)=>{
+    return i!= id;
+  })
+  setlistData(updatedListData);
+}
+
+function removeAll(){
+   setlistData([]);
+}
+    
+  return (
+    <>
+    <div className="container">
+       <div className="header">TODO List</div>
+       <input type="text" placeholder='Add Activity' value={activity} onChange={(e)=> setActivity(e.target.value)}/>
+       <button onClick={addActivity}>Add</button>
+       <p className='list-heading'>Here is your List :{")"}</p>
+       {listData!=[] && listData.map((data, i)=>{
+        return(
+            <>
+            <p key={i} >
+                <div className='listData'>{data}</div>
+                <div className='btn-position'>
+                <button onClick={()=>removeActivity (i)}>Remove</button>
+                </div>
+                
+            </p>
+            </>
+        )
+       })
+
+       }
+       {listData.length>=1 &&  
+       <button onClick={removeAll}>Remove All</button> }
+       
+    </div>
+    </>
+  )
+}
+
+export default TodoList
